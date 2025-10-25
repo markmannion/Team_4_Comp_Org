414cb5920fbc93842860f9287a202b6ae51fe6ac
Mark_Mannion <markj.mannion@icloud.com>
2025-10-25 20:02:43 +0100

    Fixed switch case, updated methods, updated to correct VESA codes

--- src/main/java/code/DisplayInfo.java
+++ src/main/java/code/DisplayInfo.java
@@ -24,13 +24,8 @@ public class DisplayInfo {
             Display display = displays.get(d);
             byte[] edidData = display.getEdid();
 
-
-
             System.out.println("Monitor No: " + (d+1));
             System.out.println();
-
-
-
             if (edidData == null || edidData.length == 0){
                 System.out.println("There's no EDID info");
                 System.out.println();
@@ -39,20 +34,23 @@ public class DisplayInfo {
 
             System.out.println("Screen Width: " + EdidUtil.getHcm(edidData));
             System.out.println("Screen Height: " + EdidUtil.getVcm(edidData));
-            //System.out.println("Preferred Resolution: " + EdidUtil.getPreferredResolution(edidData));
 
+            //Unless it's a monitor it won't print a valid resolution/refresh rate
+            System.out.println("Preferred Resolution: " + EdidUtil.getTimingDescriptor(edidData));
 
             System.out.println("Manuafacturer ID: " + EdidUtil.getManufacturerID(edidData));
             System.out.println("Display ID: " + EdidUtil.getProductID(edidData));
             System.out.println("Serial Number: " + EdidUtil.getSerialNo(edidData));
             System.out.println("EDID Version: " + EdidUtil.getVersion(edidData));
-            //System.out.println("Display Model: " + EdidUtil.getModel(edidData));
 
-            System.out.println("Manuafacture Week: " + EdidUtil.getWeek(edidData));
+            //not really needed as descriptorType 0xFC prints a valid display name
+            System.out.println("Display Model: " + EdidUtil.getDescriptorText(edidData));
+
+            System.out.println("Manuafacture Week: " + EdidUtil.getWeek(edidData)); //laptops don't have any week info
             System.out.println("Manuafacture Year: " + EdidUtil.getYear(edidData));
             System.out.println("Is this a digital display? " + (EdidUtil.isDigital(edidData) ? "Yes": "No"));
 
-            byte[][] descriptionBlocks = EdidUtil.getDescriptors(edidData);
+            byte[][] descriptionBlocks = EdidUtil.getDescriptors(edidData); //has to be a 2d array as per api
             if(descriptionBlocks!= null && descriptionBlocks.length > 0){
                 System.out.println();
                 System.out.println("VESA Display Descriptors: ");
@@ -60,23 +58,25 @@ public class DisplayInfo {
                     byte[] descriptor = descriptionBlocks[i];
                     int type = EdidUtil.getDescriptorType(descriptor);
 
-
-                    //need to figure out proper vesa edid descriptor types for switch case to work properly
+                    //descriptor codes for display info (18 byte blocks = descriptors)
+                    //https://en.wikipedia.org/wiki/Extended_Display_Identification_Data
                     String descriptorType;
                     switch(type){
-                        case monitorName:
-                            descriptorType = "Monitor Name";
+                        case 0xFC:
+                            descriptorType = "Display Name";
                             break;
-                        case serialNo:
+                        case 0xFF:
                             descriptorType = "Serial Number";
                             break;
-                        case rangeLimits:
-                            descriptorType = "Range Limits";
+                        case 0xFD:
+                            descriptorType = "Range Limits"; //refresh rate but is known as range limits in VESA EDID
                             break;
                         default:
                             descriptorType = "Other Info";
                             break;
                     }
+                    String descriptorText = EdidUtil.getDescriptorText(descriptor);
+                    System.out.println(descriptorType + " : " + descriptorText);
                 }
             }
 
