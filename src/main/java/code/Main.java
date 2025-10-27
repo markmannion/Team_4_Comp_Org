ac9bcdd3a3da0a5dde037be4126933eb410dcf5c
Hazel <chappell.hazel1@gmail.com>
2025-10-27 22:05:52 +0000

    Network Info

--- src/main/java/code/Main.java
+++ src/main/java/code/Main.java
@@ -8,6 +8,7 @@ Main {
     public static void main(String[] args) {
         SystemManager manager = new SystemManager();
         CpuInfo cpuInfo = new CpuInfo(manager.getHal());
-
+        new BatteryInfo().getBatteryInfo(manager.getHal());
+        new NetworkInfo().getNetworkInfo(manager.getHal());
     }
 }
---
+++ src/main/java/code/NetworkInfo.java
@@ -0,0 +1,39 @@
+package code;
+
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.hardware.NetworkIF;
+import java.util.Arrays;
+import java.util.List;
+
+public class NetworkInfo {
+    public void getNetworkInfo(HardwareAbstractionLayer hal){
+        List<NetworkIF> networkList = hal.getNetworkIFs();
+        int counter = 1;
+        for (NetworkIF network:networkList){
+            System.out.println("~~~~ NETWORK "+counter+" ~~~~");
+            counter++;
+            String name = network.getName(); //Network interface name
+            String description = network.getDisplayName(); //Network interface description
+            String addressV4 = Arrays.toString(network.getIPv4addr()); //IP v4 address,  unique identifier (decimal)
+            String addressV6 = Arrays.toString(network.getIPv6addr()); //IP v6 address, unique identifier (hex)
+            long byteSent = network.getBytesSent(); //Bytes sent over network
+            long byteReceived = network.getBytesRecv(); //Bytes received over network
+            long packetSent = network.getPacketsSent(); //Packets sent over network
+            long packetReceived = network.getPacketsRecv(); //Packets received over network
+
+            System.out.println("name: "+name);
+            System.out.println("description: "+description);
+            System.out.println("Internet Protocol V6 Address: "+addressV6);
+
+            if (addressV4 != "[]"){ //Check if IP v4 address is null
+                System.out.println("Internet Protocol V4 Address: "+addressV4);
+            }
+
+            System.out.println("Bytes Sent: "+byteSent);
+            System.out.println("Bytes Received: "+byteReceived);
+            System.out.println("Packets Sent: "+packetSent);
+            System.out.println("Packets Received: "+packetReceived);
+            System.out.println(" ");
+        }
+    }
+}
