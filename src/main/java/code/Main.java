dbd95ae3def9de2cb28c2d36183d5ab6dd7bebf7
jude2k6 <judedelaney06@gmail.com>
2025-10-28 15:27:41 +0000

    added memory to menu, fixed menu layout

--- src/main/java/code/Main.java
+++ src/main/java/code/Main.java
@@ -1,11 +1,12 @@
 package code;
+
 import java.io.IOException;
 import java.util.Scanner;
 
 
 public class
 Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in); //declaring scanner
         SystemManager manager = new SystemManager(); //system info
 
@@ -13,18 +14,19 @@ Main {
         CpuInfo cpuInfo = new CpuInfo(manager.getHal());
 
         GraphicsCardInfo gpuInfo = new GraphicsCardInfo(manager.getHal());
+        MemoryInfo memoryInfo = new MemoryInfo(manager.getHal());
         DisplayInfo displayInfo = new DisplayInfo(manager.getHal());
         BaseBoardInfo motherboardInfo = new BaseBoardInfo(manager.getHal());
         UsbInfo usbInfo = new UsbInfo(manager.getHal());
         PciInfo pciInfo = new PciInfo();
 
 
-        while(true){
+        while (true) {
             System.out.println("Welcome to SysInfo : Team 4");
             System.out.println("Choose which information to display: ");
             System.out.println("1. CPU Information");
             System.out.println("2. GPU Information");
-            System.out.println("3. OS Information");
+            System.out.println("3. Memory Information");
             System.out.println("4. Motherboard Information");
             System.out.println("5. Display Information");
             System.out.println("6. USB Information");
@@ -35,77 +37,111 @@ Main {
             System.out.print("---> ");
 
             int choice = sc.nextInt();
-            switch (choice) {
-                case 1:{
-                    System.out.println("Which cpu information would you like to display? ");
-                    System.out.println("1. General Information");
-                    System.out.println("2. Cache Information");
-                    int choice2 = sc.nextInt();
-                    switch (choice2){
-                        case 1:
-                            cpuInfo.getCpuSummary();
+            int menu = 0;
+            while (true) {
+                switch (choice) {
+                    case 1: {
+                        System.out.println("Which cpu information would you like to display? ");
+                        System.out.println("1. General Information");
+                        System.out.println("2. Cache Information");
+                        int choice2 = sc.nextInt();
+                        switch (choice2) {
+                            case 1:
+                                cpuInfo.getCpuSummary();
+                                System.out.println("-------------------------");
+
+                                break;
+                            case 2:
+                                cpuInfo.getCaches();
+                                System.out.println("-------------------------");
+
+                                break;
+                        }
+
                         break;
-                        case 2:
-                            cpuInfo.getCaches();
-                            break;
+                    }
+                    case 2: {
+                        gpuInfo.printOutGpuInfo();
+                        System.out.println("-------------------------");
+                        break;
+                    }
+                    case 3: {
+                        System.out.println("Which Ram information would you like to display? ");
+                        System.out.println("1. Physical Ram ");
+                        System.out.println("2. Virtual Ram");
+                        int choice2 = sc.nextInt();
+                        System.out.println();
+                        switch (choice2) {
+                            case 1:
+                                memoryInfo.getMemoryInfo();
+                                System.out.println("-------------------------");
+
+                                break;
+                            case 2:
+                                memoryInfo.getVirtualMemoryInfo();
+                                System.out.println("-------------------------");
+
+                                break;
+
+                        }
+                        break;
                     }
 
-                    break;}
-                case 2:{
-                    gpuInfo.printOutGpuInfo();
-                    System.out.println("-------------------------");
-                    break;}
-                case 3:
-                    //OSInfo method
-                    //System.out.println("-------------------------");
-                    //break;
-                case 4:{
-                    System.out.println("Which motherboard information would you like to display? ");
-                    System.out.println("1. General Information");
-                    System.out.println("2. Firmware Information");
-                    int choice2 = sc.nextInt();
-                    System.out.println();
-                    switch (choice2) {
-                        case 1:
-                            motherboardInfo.printOutBaseBoardInfo();
-                            System.out.println("-------------------------");
-                            break;
-                        case 2:
-                            motherboardInfo.printOutFirmwareInfo();
-                            System.out.println("-------------------------");
-                            break;
-                        default:
-                            System.out.println("Please enter a valid number");
-                            System.out.println("-------------------------");
-                            break;}
+                    case 4: {
+                        System.out.println("Which motherboard information would you like to display? ");
+                        System.out.println("1. General Information");
+                        System.out.println("2. Firmware Information");
+                        int choice2 = sc.nextInt();
+                        System.out.println();
+                        switch (choice2) {
+                            case 1:
+                                motherboardInfo.printOutBaseBoardInfo();
+                                System.out.println("-------------------------");
+                                break;
+                            case 2:
+                                motherboardInfo.printOutFirmwareInfo();
+                                System.out.println("-------------------------");
+                                break;
+                            default:
+                                System.out.println("Please enter a valid number");
+                                System.out.println("-------------------------");
+                                break;
+                        }
+
+                        break;
                     }
-                case 5:
-                    displayInfo.printOutDisplayInfo();
-                    System.out.println("-------------------------");
-                    break;
-                case 6:
-                    usbInfo.printOutInfo();
-                    System.out.println("-------------------------");
-                    break;
-                case 7:
-                    pciInfo.printOutInfo();
-                    System.out.println("-------------------------");
-                    break;
-                case 8:
-                    new BatteryInfo().getBatteryInfo(manager.getHal());
-                    System.out.println("-------------------------");
-                    break;
-                case 9:
-                    new NetworkInfo().getNetworkInfo(manager.getHal());
-                    System.out.println("-------------------------");
-                    break;
-                case 10:
-                    System.out.println("Exiting SysInfo");
-                    System.out.println("-------------------------");
-                    sc.close();
-                    return;
-                default:
-                    System.out.println("Please choose a valid option");
+                    case 5:
+                        displayInfo.printOutDisplayInfo();
+                        System.out.println("-------------------------");
+                        break;
+                    case 6:
+                        usbInfo.printOutInfo();
+                        System.out.println("-------------------------");
+                        break;
+                    case 7:
+                        pciInfo.printOutInfo();
+                        System.out.println("-------------------------");
+                        break;
+                    case 8:
+                        new BatteryInfo().getBatteryInfo(manager.getHal());
+                        System.out.println("-------------------------");
+                        break;
+                    case 9:
+                        new NetworkInfo().getNetworkInfo(manager.getHal());
+                        System.out.println("-------------------------");
+                        break;
+                    case 10:
+                        System.out.println("Exiting SysInfo");
+                        System.out.println("-------------------------");
+                        sc.close();
+                        return;
+                    default:
+                        System.out.println("Please choose a valid option");
+                }
+
+                System.out.println("Press 1 to return to menu");
+                menu= sc.nextInt();
+                if(menu ==1) break;
             }
         }
     }
--- src/main/java/code/MemoryInfo.java
+++ src/main/java/code/MemoryInfo.java
@@ -4,6 +4,7 @@ import oshi.hardware.GlobalMemory;
 import oshi.hardware.HardwareAbstractionLayer;
 import oshi.hardware.PhysicalMemory;
 import oshi.hardware.VirtualMemory;
+
 import java.util.List;
 
 public class MemoryInfo {
@@ -13,7 +14,7 @@ public class MemoryInfo {
     VirtualMemory virtualMemory;
 
 
-    MemoryInfo(HardwareAbstractionLayer hal){
+    MemoryInfo(HardwareAbstractionLayer hal) {
         GlobalMemory memory = hal.getMemory();
         physicalMemoryList = memory.getPhysicalMemory();
         virtualMemory = memory.getVirtualMemory();
@@ -21,38 +22,34 @@ public class MemoryInfo {
     }
 
 
-
-
-    public void getTotalMemoryBytes(){
+    public void getTotalMemoryBytes() {
         System.out.print(memory.getTotal());
 
     }
 
-    public long getTotalMemoryGB(){
-        return (memory.getTotal()/1000000000);
+    public long getTotalMemoryGB() {
+        return (memory.getTotal() / 1000000000);
     }
 
-    public void getMemoryInfo(){
-        try{System.out.println("Manufacturer: "+ physicalMemoryList.get(0).getManufacturer());
-            System.out.println("Capacity: "+ getTotalMemoryGB() );
-            System.out.println("Clock Speed: " + physicalMemoryList.get(0).getClockSpeed()/1000000);
-            System.out.println("Part Number: "+ physicalMemoryList.get(0).getPartNumber());
+    public void getMemoryInfo() {
+        try {
+            System.out.println("Manufacturer: " + physicalMemoryList.get(0).getManufacturer());
+            System.out.println("Capacity: " + getTotalMemoryGB());
+            System.out.println("Clock Speed: " + physicalMemoryList.get(0).getClockSpeed() / 1000000);
+            System.out.println("Part Number: " + physicalMemoryList.get(0).getPartNumber());
             System.out.println("Page size" + memory.getPageSize());
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Youre better than everyone ");
         }
 
 
     }
-    public void getVirtualMemoryInfo(){
+
+    public void getVirtualMemoryInfo() {
         System.out.println("Max Virtual Memory: " + virtualMemory.getVirtualMax());
-        System.out.println("Virtual Memory Inuse: "+ virtualMemory.getVirtualInUse() );
+        System.out.println("Virtual Memory Inuse: " + virtualMemory.getVirtualInUse());
 
     }
-
-
-
 
 
 }
