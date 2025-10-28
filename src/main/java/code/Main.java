172cc546ca253d9dfddf6f3e7e78a350f0a82efc
Hazel <chappell.hazel1@gmail.com>
2025-10-28 09:11:53 +0000

    Main Update

--- src/main/java/code/Main.java
+++ src/main/java/code/Main.java
@@ -6,9 +6,6 @@ import oshi.hardware.CentralProcessor;
 public class
 Main {
     public static void main(String[] args) {
-        SystemManager manager = new SystemManager();
-        CpuInfo cpuInfo = new CpuInfo(manager.getHal());
-        new BatteryInfo().getBatteryInfo(manager.getHal());
-        new NetworkInfo().getNetworkInfo(manager.getHal());
+
     }
 }
