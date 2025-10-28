4984735d18c2542d3e4308c5930918bc5ea208f3
vadim <poipooo600@gmail.com>
2025-10-28 00:12:03 +0000

    Pci finished

---
+++ src/main/java/code/PciInfo.java
@@ -0,0 +1,27 @@
+package code;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PciInfo {
+
+    public static void getInfo() throws IOException {
+        ProcessBuilder pb = new ProcessBuilder("lspci");
+        Process process = pb.start();
+
+        BufferedReader buffer= new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = buffer.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        getInfo();
+
+    }
+}
