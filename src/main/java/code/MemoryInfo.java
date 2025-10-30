42ef8580e665b74e2066cd0619c3315be200708c
jude2k6 <judedelaney06@gmail.com>
2025-10-30 11:28:12 +0000

    fixed some fomatting

--- src/main/java/code/MemoryInfo.java
+++ src/main/java/code/MemoryInfo.java
@@ -34,20 +34,21 @@ public class MemoryInfo {
     public void getMemoryInfo() {
         try {
             System.out.println("Manufacturer: " + physicalMemoryList.get(0).getManufacturer());
-            System.out.println("Capacity: " + getTotalMemoryGB());
-            System.out.println("Clock Speed: " + physicalMemoryList.get(0).getClockSpeed() / 1000000);
+            System.out.println("Capacity: " + getTotalMemoryGB() + "Gb");
+            System.out.println("Clock Speed: " + physicalMemoryList.get(0).getClockSpeed() / 1000000 +"Mhz");
             System.out.println("Part Number: " + physicalMemoryList.get(0).getPartNumber());
-            System.out.println("Page size" + memory.getPageSize());
+            System.out.println("Page size: " + memory.getPageSize());
         } catch (Exception e) {
-            System.out.println(getTotalMemoryGB());
+            System.out.println("Capacity: "+getTotalMemoryGB()+"GB");
+            System.out.println("Linux some info is restricted");
         }
 
 
     }
 
     public void getVirtualMemoryInfo() {
-        System.out.println("Max Virtual Memory: " + virtualMemory.getVirtualMax());
-        System.out.println("Virtual Memory Inuse: " + virtualMemory.getVirtualInUse());
+        System.out.println("Max Virtual Memory: " + virtualMemory.getVirtualMax()/1024/1024/1024+ "GB");
+        System.out.println("Virtual Memory Inuse: " + virtualMemory.getVirtualInUse()/1024/1024/1024 + "GB" );
     }
 
     public int memoryUtilPercent() {
